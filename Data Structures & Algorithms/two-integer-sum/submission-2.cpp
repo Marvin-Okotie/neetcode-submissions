77ef5b2d43de65cb333a416287a119e6d21a2053
Marvin Okotie <155176572+Marvin-Okotie@users.noreply.github.com>
2026-07-23 12:22:13 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // Since we are told there's always a pair that satisfies the condition - early stoppage
+        if(nums.size() == 2) return{0, 1}; 
+        
+        unordered_map<int, int> comp_idx;
+
+        for(int i = 0; i < nums.size(); ++i){
+            int complement = target - nums[i];
+            auto it = comp_idx.find(complement);
+            if(it != comp_idx.end()){
+                return {it->second, i};
+            }
+            comp_idx[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
