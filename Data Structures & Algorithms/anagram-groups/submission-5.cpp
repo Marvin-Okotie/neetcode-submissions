afcff01f0b93c5c771d2ceb4afc720a1b42b54c6
Marvin Okotie <155176572+Marvin-Okotie@users.noreply.github.com>
2026-07-23 15:15:13 -0400

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> str_map;
+        str_map.reserve(strs.size());
+
+        for(auto& str: strs){ 
+        string key(26,'\0');
+            for(char c : str){ 
+                ++key[c - 'a']; //Use array of chars(int/ascii representation) in the string as map key
+            }
+            str_map[key].push_back(move(str)); 
+        }
+
+        for(auto& p: str_map){
+            ans.push_back(move(p.second));
+        }
+
+        return ans;
+    }
+};
